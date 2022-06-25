82a72e9b5053c8d2385c5beeed0cc8838a32074e
Xenon <SDA152nn@gmail.com>
2022-06-25 14:32:41 +0200

    creating Game 2

--- js/game-2/config.js
+++ js/game-2/config.js
@@ -10,7 +10,38 @@ class Questions {
             "c": c,
             "d": d,
         };
+        this.result = '';
+    }
+    /**
+     * Функция ввода ответа пользователя
+     */
+    prompt() {
+        this.result = prompt(`${this.text}\n
+        A:  ${(this.responseOptions.a)}          B:  ${(this.responseOptions.b)}\n
+        C:  ${(this.responseOptions.c)}          D:  ${(this.responseOptions.d)}`);
+    }
+    /**
+     * Функция проверки ответа
+     */
+    check() {
+        if (this.result.toLocaleLowerCase() == this.rightAnswer) {
+            alert(`Вы угадали! Ваш счёт ${score += 100}`)
+        } else {
+            alert(`Вы не угадали! Ваш счёт ${reducingScore(score)}`)
+        }
     }
 }
 
 let score = 0
+/**
+ * Функция уменьшения счёта. Если больше 50, то уменьшает его на 50 очков.
+ * @param {Number} score Количество очков.
+ * @returns Возвращает или 0 очкво или меньше на 50 очков.
+ */
+function reducingScore(score) {
+    if (score <= 50) {
+        return 0;
+    } else {
+        return score -= 50;
+    }
+}
